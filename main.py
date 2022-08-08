f37e5ffbc7c6810ad6ae72f040f03ee53e2e5fce
Ramon Balaguer <rbalaguer@voxelgroup.net>
2022-08-08 11:15:47 +0200

    feat: add more commands

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ class Person:
     def get_age(self) -> int:
         return self._age
 
-    def birth_month(self) -> int:
+    def month(self) -> int:
         return self._birth_month
+
+    def name(self) -> str:
+        return self._name
 
     def __str__(self) -> str:
         return f"{self._name} {self._age}"
@@ -33,33 +36,86 @@ class Month:
 
 
 class Friends:
-    MONTHS = Enumerable([Month(2, "Febrero"), Month(8, "Agosto")])
+    MONTHS = Enumerable([Month(1, "Enero"), Month(2, "Febrero"), Month(8, "Agosto")])
 
     def __init__(self):
         ramon = Person("Ramon", 36, 2)
-        victor = Person("Victor", 29, 8)
+        victor = Person("Victor", 30, 8)
         laura = Person("Laura", 29, 2)
-        self._persons = Enumerable([ramon, victor, laura])
+        paco = Person("Paco", 93, 1)
+        carmen = Person("Carmen", 18, 2)
+        self._persons = Enumerable([ramon, victor, laura, paco, carmen])
+
+    def __str__(self) -> str:
+        return ", ".join(self._persons.select(lambda friend: friend.__str__()).to_list())
+
+    def order_by_age(self):
+        self._persons = self._persons.order_by(lambda friend: friend.get_age())
 
     def oldest(self) -> Person:
         return self._persons.order_by_descending(lambda friend: friend.get_age()).first()
 
-    def group_by(self):
-        return self._persons.group_by(["birth_month"], lambda friend: friend.birth_month())
+    def group_by_birth_month(self) -> Enumerable:
+        return self._persons.group_by(["birth_month"], lambda friend: friend.month())
 
-    def add_months_name(self):
+    def group_by_month(self):
         return self.MONTHS.group_join(self._persons,
                                       lambda month: month.month_numer_of_the_year,
-                                      lambda friend: friend.birth_month(),
+                                      lambda friend: friend.month(),
                                       lambda friend_month: friend_month)
 
-    def __str__(self) -> str:
-        return "\n".join(self._persons.select(lambda friend: friend.__str__()).to_list())
+    def avg_age(self):
+        return self._persons.avg(lambda friend: friend.get_age())
+
+    def number_of_friends(self):
+        return self._persons.count()
+
+    def new_relationship(self, friend: Person):
+        self._persons.add(friend)
+
+    def born_in_february(self):
+        return self._persons.where(lambda friend: friend.month() == 2)
+
+    def born_in_month(self, month: str) -> Enumerable:
+        mont_object: Month = self.MONTHS.where(lambda month_item: month_item.name == month).single()
+        return self._persons.where(lambda friend: friend.month() == mont_object.month_numer_of_the_year)
+
+    def bring_friends_together(self, other_friends: Enumerable):
+        self._persons = self._persons.concat(other_friends)
+
+    def mutual_friends_with_the_same_age(self, other_friends: Enumerable) -> Enumerable:
+        return self._persons.join(other_friends,
+                                  lambda friend: friend.get_age(),
+                                  lambda friend: friend.get_age(),
+                                  lambda result: result)
 
 
 if __name__ == '__main__':
     friends = Friends()
     print(friends)
+    friends.order_by_age()
+    print(friends)
     print(friends.oldest())
-    print(friends.group_by())
-    print(friends.add_months_name())
+    print(friends.group_by_birth_month())
+    print(friends.group_by_month())
+    print(friends.avg_age())
+    print(friends.number_of_friends())
+
+    carlos = Person("Carlos", 45, 8)
+    friends.new_relationship(carlos)
+    print(friends.number_of_friends())
+    print(friends)
+    print(friends.avg_age())
+
+    print(friends.born_in_february())
+    print(friends.born_in_month("Febrero"))
+    print(friends.born_in_month("Enero"))
+
+    pepe = Person("pepe", 36, 2)
+    manuela = Person("manuela", 30, 8)
+    cristina = Person("cristina", 29, 2)
+    extra_friends = Enumerable([pepe, manuela, cristina])
+    print(friends.mutual_friends_with_the_same_age(extra_friends))
+
+    friends.bring_friends_together(extra_friends)
+    print(friends)
